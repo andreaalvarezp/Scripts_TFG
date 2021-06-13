e0c49123d3f41f4465d737960c805f95f1893726
andreaalvarezp <67371059+andreaalvarezp@users.noreply.github.com>
2021-06-13 18:56:48 +0200

    Update gilbert.sh

--- gilbert.sh
+++ gilbert.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 #
-#SBATCH --job-name=ct
-#SBATCH --mem=220GB
+#SBATCH --job-name=gilbert
+#SBATCH --mem=18GB
 #SBATCH -n 20
-#SBATCH -p bigmem
+#SBATCH -p medium
 #SBATCH --ntasks=1
-#SBATCH --time=200:00:00
+#SBATCH --time=20:00:00
 
 # Defino variables
 resources="/home/aalvarez/bbmap/resources"
 bbtool="/home/aalvarez/bbmap"
 db="/home/aalvarez/db"
 ct="/home/aalvarez/Ct_gilbert"
-BB="/home/aalvarez/Ct_gilbert/BBTools"
-output="/home/aalvarez/Ct_gilbert/output"
 query="/home/aalvarez/Ct_gilbert/paired-query"
+output="/home/aalvarez/Ct_gilbert/output"
 blast="/home/aalvarez/Ct_gilbert/blast"
 tables="/home/aalvarez/Ct_gilbert/tables"
 bowtie2="/home/aalvarez/Ct_gilbert/bowtie2"
 cutadapt="/home/aalvarez/Ct/cutadapt"
-trinity="/home/aalvarez/Ct_gilbert/trinity"
-transdecoder="/home/aalvarez/Ct_gilbert/transdecoder"
-HMMscan="/home/aalvarez/Ct_gilbert/HMMscan"
+transdecoder="/home/aalvarez/Ct_gilbert/trinity_$1.Trinity.fasta.transdecoder_dir"
+hmmer="/home/aalvarez/Ct_gilbert/hmmer"
 dir=$PWD
 
 ## Cargo módulos y activo el entorno virtual
@@ -44,27 +42,12 @@ echo "Objetivo: comprobar la fiabilidad del pipeline de procesado propuesto repi
 ## 1. SEPARAR PAIRED-ENDS EN DOS ARCHIVOS DISTINTOS
 
 $bbtool/reformat.sh in=$ct/SRA/Ct_$1_$2.fastq.gz out1=$ct/SRA/$1_$2_R1.fq.gz out2=$ct/SRA/$1_$2_R2.fq.gz
-$bbtool/reformat.sh in=$ct/SRA/$1Ct_$2.fastq.gz out1=$ct/SRA/$1_$2_R1.fq.gz out2=$ct/SRA/$1_$2_R2.fq.gz
 
 ## 2. QUITO ADAPTADORES CON CUTADAPT
 
-module load Miniconda3/4.7.10
-source activate AndreaConda
-
-cutadapt -o $cutadapt/$1_1.fq.gz -p $cutadapt/$1_2.fq.gz $paired/$1_R1.fq.gz $paired/$1_R2.fq.gz
+cutadapt -o $cutadapt/$1_1.fq.gz -p $cutadapt/$1_2.fq.gz $query/$1_R1.fq.gz $query/$1_R2.fq.gz
 
 # Retirar los adaptadores con cutadapt sin conocer los parámetros exactos del trabajo de Gilbert dio lugar a archivos del mismo tamaño.
-# ALTERNATIVA: QUITARLOS CON BBTOOLS. Solo se llevaron a cabo los pasos de agrupación para reducir el tamaño de los archivos y el tiempo de computación posterior, así como la eliminación de los adapatadores con BBDuk.
-
-# Sketch
-$bbtool/sendsketch.sh in=$query/Ct_$1.fastq.gz out=$BB/sketch_Ct_$1.txt reads=200000 merge printname0=f records=20 overwrite=true color=false depth depth2 volume sortbyvolume contam2=genus nt ow
-
-# Clumpify
-$bbtool/clumpify.sh -Xmx100G pigz=t unpigz=t zl=4 reorder in1=$paired/$1_R1.fq.gz in2=$paired/$1_R2.fq.gz out1=$BB/$1_clump1.fq.gz out2=$BB/$1_clump2.fq.gz passes=1
-
-# BBDuk
-# 1.1. Retiro adaptadores
-$bbtool/bbduk.sh -Xmx75G -threads=15 ktrim=r ordered minlen=10 mink=10 tbo tpe rcomp=t overwrite=true k=15 hdist=1 hdist2=1 zl=4 ow=true in1=$BB/$1_clump1.fq.gz in2=$BB/$1_clump2.fq.gz out1=$BB/$1_temp1.fq.gz out2=$BB/$1_temp2.fq.gz rqc=hashmap outduk=$BB/output/$1_ktrim_kmerStats.txt stats=$BB/output/$1_ktrim_scaffoldStats.txt loglog ref=$resources/adapters.fa
 
 ## 3. BOWTIE2
 
@@ -74,19 +57,23 @@ bowtie2-build -f $genome/Colletotrichum_genome.fna $bowtie2/Ct_bt2base # El geno
 
 # 3.2. BOWTIE2 - ALINEAMIENTO: Se utilizó la opción --un para poder conservar aquellas secuencias que no alineaban.
 
-bowtie2 -x $bowtie2/genome/Ct_bt2base -1 $BB/$1_temp1.fq.gz -2 $BB/$1_temp2.fq.gz --un-conc-gz $bowtie2/$1_align.fq.gz
+bowtie2 -x $bowtie2/genome/Ct_bt2base -1 $query/$1_R1.fq.gz -2 $query/$1_R2.fq.gz --un-conc-gz $bowtie2/$1_align.fq.gz
+
+# MODIFICACIÓN PIPELINE: NORMALIZACIÓN
+
+$bbtool/bbnorm.sh -Xmx150G -Xms75G -threads=15 in1=$bowtie2/$1_align.1.fq.gz in2=$bowtie2/$1_align.2.fq.gz out1=$bowtie2/$1_align_norm.1.fq.gz out2=$bowtie2/$1_align_norm.2.fq.gz
 
 # 4. ENSAMBLAJE DE NOVO CON TRINITY
 
 echo "Sample $1: ensamblaje de novo con Trinity"
 
-Trinity --seqType fq --max_memory 200G --CPU 40 --left $bowtie2/$1_align.fq.1.gz --right $bowtie2/$1_align.fq.2.gz --output $ct/trinity_out2 --full_cleanup --verbose
+Trinity --seqType fq --max_memory 200G --CPU 40 --left $bowtie2/$1_align.fq.1.gz --right $bowtie2/$1_align.fq.2.gz --output $ct/trinity_$1 --full_cleanup --verbose
 
 # 6. IDENTIFICACIÓN DE REGIONES CODIFICANTES CON TRANSDECODER
 
 # Para la instalación de TransDecoder es necesario hacer uso del entorno virtual
 
-TransDecoder.LongOrfs -t trinity_out2.Trinity.fasta
+TransDecoder.LongOrfs -t trinity_$1.Trinity.fasta
 
 # 7. IDENTIFICACIÓN DE SECUENCIAS CON SIMILITUD LIMITADA A DOMINIOS DE PROTEÍNAS VIRALES CON HMMER
 
@@ -108,15 +95,15 @@ hmmpress RdRP.hmm
 
 # 7.4. Busco las proteínas con hmmscan
 
-hmmscan RdRP.hmm $ct/trinity_out2.Trinity.fasta.transdecoder_dir/longest_orfs.pep > $ct/hmmer/hmmer_results
+hmmscan RdRP.hmm $transdecoder/longest_orfs.pep > $hmmer/hmmer_$1
 
 # 8. COMPROBACIÓN DE LOS RESULTADOS CON BLAST
 # 8.1. Creo un archivo oneline de los ensamblajes de Trandecoder y creo una lista manual con los identificadores de los hit de HMMER
 # Con ese resultado obtengo el archivo FASTA con las proteínas que han dado hit
 
-awk '{if($0 ~ /^>/){print $0} else {printf $0}}' $ct/transdecoder/longest_orfs.pep | perl -pe "s/>/\n>/g" > $ct/transdecoder/longest_orfs_oneline.fasta
-grep -A1 --no-group-separator -f $ct/hmmer/list1.txt $ct/transdecoder/longest_orfs_oneline.fasta > $ct/hmmer/hmmer-results.fasta
+awk '{if($0 ~ /^>/){print $0} else {printf $0}}' $transdecoder/longest_orfs.pep | perl -pe "s/>/\n>/g" > $transdecoder/longest_orfs_oneline.fasta
+grep -A1 --no-group-separator -f $hmmer/list1_$1.txt $transdecoder/longest_orfs_oneline.fasta > $hmmer/hmmer-$1.fasta
 
 # 9. COMPROBACIÓN DE RESULTADOS: BLAST CONTRA NCBI nr
 
-diamond blastp -d $db/NEWNR/nr -q $ct/hmmer/hmmer-hits.fasta -o $ct/hmmer-NCBI.blast -f 0 -k 1 -e 0.001 --unal 0 --more-sensitive
+diamond blastp -d $db/NEWNR/nr -q $ct/hmmer/hmmer-$1.fasta -o $ct/hmmer-NCBI_$1.blast -f 0 -k 2 -e 0.001 --unal 0 --more-sensitive
